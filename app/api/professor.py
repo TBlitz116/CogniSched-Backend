8a782323b9509e50a753843171733828a612cb04
Tom Mulamoottil <tomm1@umbc.edu>
2026-05-08 13:28:54 -0400

    added in fixes

--- app/api/professor.py
+++ app/api/professor.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from datetime import datetime
+from datetime import datetime, timedelta
 from pydantic import BaseModel
 from app.core.database import get_db
 from app.api.deps import require_role
